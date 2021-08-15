de2966df7d3453977e18a697eadf1694e2b26bcf
qwertyroiro <roiro8562@gmail.com>
2021-08-16 02:28:03 +0900

    08-4_end_task_3

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,4 +21,11 @@ class PostController extends Controller
     {
         return view("create");
     }
+
+    public function store(Post $post, Request $request)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect("/posts/" . $post->id);
+    }
 }
