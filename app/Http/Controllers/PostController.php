cef44b3a30950bbf3789e545833be1b09c98c728
qwertyroiro <roiro8562@gmail.com>
2021-08-15 14:02:30 +0900

    08-2_End_Task2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,12 +5,10 @@ namespace App\Http\Controllers;
 use App\Post;
 use Illuminate\Http\Request;
 
-
 class PostController extends Controller
 {
     public function index(Post $post)
     {
         return $post->get();
     }
-    
 }
