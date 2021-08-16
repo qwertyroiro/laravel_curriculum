ba48351e965c92220521cd1a61f42636160ff7ae
qwertyroiro <roiro8562@gmail.com>
2021-08-16 14:08:24 +0900

    08-5_end_task_3to5

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,28 +10,35 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-        return view('index')->with(['posts' => $post->getPaginateByLimit()]);
+        return view('index')->with(['posts' => $post->getPaginateByLimit()]); // indexビューのpostsにPostのインスタンス$postのgetPaginateByLimitメソッドの返り値を代入して、ビューを返してる
     }
 
     public function show(Post $post)
     {
-        return view("show")->with(['post' => $post]);
+        return view("show")->with(['post' => $post]); // showビューのpostにPostのインスタンス$postを代入して、ビューを返してる
     }
 
     public function create()
     {
-        return view("create");
+        return view("create"); //createというビューを返してる
     }
 
     public function store(Post $post, PostRequest $request)
     {
-        $input = $request['post'];
-        $post->fill($input)->save();
-        return redirect("/posts/" . $post->id);
+        $input = $request['post']; // $inputにPostRequestのインスタンス$requestのpostをキーにもつリクエストパラメータを取得. createビューのタイトル&本文入力のname属性をname="post[title]"にしてるからpost？
+        $post->fill($input)->save(); // $post->create($input)でもいい
+        return redirect("/posts/" . $post->id); // リダイレクト処理(/posts/ブログid)
     }
     
     public function edit(Post $post)
     {
-        return view("edit")->with(["post" => $post]);
+        return view("edit")->with(["post" => $post]); // editビューのpostにPostのインスタンス$postを代入して、ビュー返してる
+    }
+    
+    public function update(Post $post, PostRequest $request)
+    {
+        $input = $request["post"]; // $inputにPostRequestのインスタンス$requestのpostをキーにもつリクエストパラメータを取得. editビューのタイトル&本文編集のname属性をname="post[title]"にしてるからpost？
+        $post->fill($input)->save(); // $post->create($input)でもいい // updateメソッドでは未変更で全上書きされたり、Postクラスのfillableがチェックされない
+        return redirect("/posts/" . $post->id); // リダイレクト処理(/posts/ブログid)
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/posts/{post}', 'PostController@show');
 Route::get('/posts/{post}/edit', 'PostController@edit');
 
 Route::post('/posts', 'PostController@store');
+Route::put('/posts/{post}', 'PostController@update');
